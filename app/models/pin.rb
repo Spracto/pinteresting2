b0e7e6e52cc5703e21573f5aeaf9bd64b5ad6d92
Spracto <spracto@gmail.com>
2015-01-13 11:36:06 -0800

    still trying to fix s3 error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,11 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100" }#, :default_url => "/app/images/oops, no image.jpg"
+	has_attached_file :image, 
+	:styles => { :medium => "300x300>", :thumb => "100x100" } 
+	:storage => :s3,
+	:bucket => ENV['pinternasty']
+
 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
