fc0536b873f285057853eb842fe03d19ca792735
Spracto <spracto@gmail.com>
2015-01-15 09:37:07 -0800

    POC

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, 
-	:styles => { :medium => "300x300>", :thumb => "100x100" }#,
-	#:bucket => "pinternasty"
+	:styles => { :medium => "300x300>", :thumb => "100x100" },
+	:bucket => "pinternasty"
 
 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
